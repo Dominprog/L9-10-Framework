0a59378a8c6cc496a8238f2756d290c299f1be14
murfa <mcher0ry@gmail.com>
2025-12-27 10:43:48 +0300

    Add files via upload

--- src/parsers/bodyParser.js
+++ src/parsers/bodyParser.js
@@ -1,1 +1,22 @@
-// парсеры данных
+function bodyParser(req) {
+    return new Promise((resolve) => {
+        if (req.method === 'GET' || req.method === 'DELETE') {
+            req.body = {};
+            return resolve();
+        }
+        
+        let body = '';
+        req.req.on('data', chunk => body += chunk.toString());
+        req.req.on('end', () => {
+            try {
+                req.body = body ? JSON.parse(body) : {};
+                resolve();
+            } catch {
+                req.body = {};
+                resolve();
+            }
+        });
+    });
+}
+
+module.exports = bodyParser;
